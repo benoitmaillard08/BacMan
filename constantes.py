0024378c34484c45c1ed9731fa1fdc422bd5d716
mikoou96 <ruffieux.mikael@gmail.com>
2015-11-05 19:53:30 +0100

    Début du fichier menu + ajout des images au répertoire Git

--- constantes.py
+++ constantes.py
@@ -1,8 +1,22 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 WALL = "#"
 STANDARD_PILL = "*"
 POWER_PILL = "P"
 BONUS_PILL = "B"
-=======
+#=======
 """Liste des constantes du jeu >BacMan the baccalaureates Adventure!<"""
->>>>>>> 53b5ba1d30dcf8403aeb688e8e5d284815368d0d
+#>>>>>>> 53b5ba1d30dcf8403aeb688e8e5d284815368d0d
+
+
+#------------------------------------------------------------
+# Menu
+COTE_FOND = 800 #pixels
+PATH_PIC_MAIN_MENU = 'images/main_menu.png'
+PATH_PIC_BUTTON = 'images/button.png'
+
+#Postions boutons (Tuples)
+POS_CTRL_BUT = (100, 400)
+
+DICTIO_POS_BUTS = {}
+
+#------------------------------------------------------------
--- menus.py
+++ menus.py
@@ -1,1 +1,75 @@
 """ Gestion des menus du jeu >BacMan the baccalaureates Adventure!< """
+
+# Importation de pygame et des constantes
+import pygame
+from pygame.locals import *
+import constantes
+
+class MainMenu:
+	"""
+	Classe créant les différents menus du jeu
+	"""
+
+	def __init__(self):
+		pygame.init()
+
+	def mainmenu(self):
+		"""
+		Méthode créant le menu principal
+		"""
+		# Création de la fenêtre
+		fenetre = pygame.display.set_mode((constantes.COTE_FOND, constantes.COTE_FOND), RESIZABLE)
+
+		# Importation des images
+		# Menu
+		background = pygame.image.load(constantes.PATH_PIC_MAIN_MENU).convert()
+		fenetre.blit(background,(0,0))
+
+		###########
+		# Boutons #
+		###########
+
+		# Controles
+
+		button_ctrl = pygame.image.load(constantes.PATH_PIC_BUTTON).convert_alpha()
+		# Positionnement du bouton dans la fenêtre
+		fenetre.blit(button_ctrl, constantes.POS_CTRL_BUT)
+
+		#---------------
+
+		# Rafrachissement de l'écran
+		pygame.display.flip()
+
+
+		flag = 1
+
+		while flag:
+			for event in pygame.event.get(): 	# Parcours la liste des éléments reçus
+				if event.type == QUIT:
+					flag = 0
+				elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+					x = event.pos[1] 	#Les positions du clic de la souris sont enregistrés pour être analyser
+					y = event.pos[0]
+
+
+
+
+		# Sortie de la boucle et fermeture de la fenêtre
+		pygame.display.quit()
+
+	def mouse_position(self, pos_x, pos_y):
+		"""
+		mouse_position(int pos_x, int pos_y) --> None.
+
+		On entre les positions de la souris dans la méthode, et celle-ci analysera sa position, pour éventuellement
+		lancer la méthode du bouton activé.
+		"""
+		x = pos_x
+		y = pos_y
+
+		
+
+				
+
+
+
