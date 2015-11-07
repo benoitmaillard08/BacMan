62bb6901aba29a748d136f0870e35de09e257967
mikoou96 <ruffieux.mikael@gmail.com>
2015-11-07 11:16:43 +0100

    Ajout des constantes du menu

--- constantes.py
+++ constantes.py
@@ -1,11 +1,13 @@
-#<<<<<<< HEAD
+"""Liste des constantes du jeu >BacMan the baccalaureates Adventure!<"""
+
+# Type de case
 WALL = "#"
 STANDARD_PILL = "*"
 POWER_PILL = "P"
 BONUS_PILL = "B"
 #=======
-"""Liste des constantes du jeu >BacMan the baccalaureates Adventure!<"""
-#>>>>>>> 53b5ba1d30dcf8403aeb688e8e5d284815368d0d
+
+
 
 
 #------------------------------------------------------------
