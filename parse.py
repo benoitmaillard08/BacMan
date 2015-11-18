659c09fa83bb9cd3fb381f5d288cd094b40b604f
blm08 <benoitleo.maillard@gmail.com>
2015-11-18 23:06:47 +0100

    Renommage de levels.py en parse.py

---
+++ parse.py
@@ -0,0 +1,10 @@
+""" Classes permettant la gestion et génération des niveaux du Jeu >BacMan the baccalaureates Adventure!< """
+
+# Importation du module pygame et du fichier contenant les constantes du jeu
+
+import pygame
+from pygame.locals import *
+import constantes
+
+
+
