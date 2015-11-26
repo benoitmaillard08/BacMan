2ed2f882129a955c4fa7bad9dd7af266f418a892
mikoou96 <ruffieux.mikael@gmail.com>
2015-11-26 21:23:03 +0100

    image

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
+from tkinter import *
+import constantes
 
+class Test(Tk):
+    def __init__(self):
+        self.window = Tk()
 
-text = [open('ressources/rules.txt', 'r').read().split('\n')]
+        background = Canvas(self.window, width=constantes.COTE_FOND, height=constantes.COTE_FOND, background='black')
 
+        pic = PhotoImage(file=constantes.PATH_PIC_PAGES)
+        background.create_image(0,0,anchor=NW,image=pic)
 
+        background.pack()
+
+        self.window.mainloop()
+    
