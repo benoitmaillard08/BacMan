b522564d23d38af059e4a11bc749f6fe2138fd8e
mikoou96 <ruffieux.mikael@gmail.com>
2015-11-30 17:37:35 +0100

    Premiers tests pour l'input box + création d'un nouveau bouton, pour affichage vertical uniquement

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 import pygame
 from pygame.locals import *
 import constantes
-from tkinter import *
 
 
-class Window(Tk):
-    def __init__(self):
-        window = Tk()
 
 class Test:
     
@@ -21,25 +17,56 @@ class Test:
 
         #Mise à jour de la page
         pygame.display.flip()
+    def compiler(self, liste):
+        """
+        compile( list liste) --> str
+    Méthode permettant de compiler la liste de charactères entrés en string."""
+        string = ''
 
+        for elt in liste:
+            string += elt
+
+        return string
+
+    def events(self):
+            
         #Gestionnaire d'événements
-        flag = 1
-        print('{', end='')
+        liste = []
 
-        while flag:
+        while 1:
             for event in pygame.event.get():    # Parcours la liste des éléments reçus
                 if event.type == QUIT:
                     flag = 0
                     
                 elif event.type == KEYDOWN:
-                    print('K_'+chr(event.key)+':"'+chr(event.key)+'"', end=',')
+                    if chr(event.key) in constantes.KEYS:
+                        liste += chr(event.key)
+                        print(Test().compiler(liste))
+                        
+                    elif event.key == 8:    # 8 --> code du backspace (pour effacer)
+                        if liste:   # On vérifie si la liste est vide, si oui, on ne fait rien
+                            del liste[-1]
+                            print(Test().compiler(liste))
+                            
+                        
+                    elif event.key == 13:       # 13 --> code du retour à la ligne (Enter)
+                        pseudo = ''
+                        for elt in liste:
+                            pseudo += elt
+                        print(Test().compiler(liste))
+                        
+                    else:
+                        print('Seuls les caractères normaux et sans accent sont acceptés!')
+                    
                     
                 elif event.type == MOUSEBUTTONDOWN and event.button == 1:
                     x = event.pos[0]    #Les positions du clic de la souris sont enregistrés pour être analyser
                     y = event.pos[1]
                 
-        print('}', end='\n')
         pygame.display.quit()
+
+Test().compiler([])
+Test().events()
 
 
 
