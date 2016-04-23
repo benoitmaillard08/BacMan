47320046cd10382cb16b49471864325c761af7d7
blm08 <benoitleo.maillard@gmail.com>
2016-04-23 11:21:02 +0200

    Add .bat

---
+++ requirements.bat
@@ -0,0 +1,5 @@
+C:\Python34\python.exe -m pip install --upgrade pip wheel setuptools
+
+rem packages spéciaux
+C:\Python34\python.exe -m pip install pygame
+C:\Python34\python.exe -m pip install requests
